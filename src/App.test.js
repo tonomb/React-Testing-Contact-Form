0c63cbbbc36ec5919e56931f947a27c7533e3ba1
antonio martinez baez <tono.mtzb@gmail.com>
2020-07-09 18:06:03 -0500

    working on mvp

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,47 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
+import { act } from "react-dom/test-utils";
 
-test("renders App without crashing", () => {
+test("User can submit form with all inputs", async () => {
   render(<App />);
+
+  const firstNameInput = screen.getByPlaceholderText(/edd/i);
+  const lastNameInput = screen.getByPlaceholderText(/burke/i);
+  const emailInput = screen.getByLabelText(/email/i);
+  const messageInput = screen.getByLabelText(/message/i);
+  const submitBtn = screen.getByRole('button');
+
+  
+  
+  act(()=>{
+    fireEvent.change(firstNameInput, {target: {value: 'Wade'}})
+    fireEvent.change(lastNameInput, {target: {value: 'Watts'}})
+    fireEvent.change(emailInput, {target: {value: 'wade@test.com'}})
+    fireEvent.change(messageInput, {target: {value: 'ready player one'}})
+    fireEvent.click(submitBtn)
+    })
+
+    //these tests are not working
+    const successMessage = await screen.findByTestId('result');
+    expect(successMessage).toBeInTheDocument();
+
+    console.log(successMessage)
+
+    // console.log(successMessage);
 });
+
+
+// test('Form errors on empty required values', ()=>{
+//   render(<App />);
+
+//   const submitBtn = screen.getByRole("button", /submit/i)
+//   console.log(submitBtn);
+//   fireEvent.click(submitBtn);
+
+  
+//   // const fnameError = screen.getAllByText(/Looks like there was an error: required/i);
+//   // console.log(fnameError);
+  
+
+// })
